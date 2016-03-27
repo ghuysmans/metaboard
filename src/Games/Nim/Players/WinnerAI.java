c4d1fa1e1d70bebfb8e6e163724c8060bc8f066f
radioGiorgio <florent.delgrange@gmail.com>
2016-03-27 23:20:50 +0200

    WinnerAI is now a RandomAI

--- src/Games/Nim/Players/WinnerAI.java
+++ src/Games/Nim/Players/WinnerAI.java
@@ -3,14 +3,13 @@ package Games.Nim.Players;
 import Core.NameAvatar;
 import Games.Nim.Moves.Move;
 import Games.Nim.Moves.MoveToken;
-import Games.Nim.Player;
 
 import java.util.List;
 
 /**
  * This AI always plays the best hit
  */
-public class WinnerAI extends Player {
+public class WinnerAI extends RandomAI{
 
     /**
      * Creates a player the standard way.
@@ -26,7 +25,7 @@ public class WinnerAI extends Player {
         int maxLeap = getMaxLeap();
         int position = getBoard().getTokenPosition().getI();
         if(position%(maxLeap+1) == 0)
-            return new MoveToken(1);
+            return super.pickMove();
         return new MoveToken(position % (maxLeap + 1));
     }
 
