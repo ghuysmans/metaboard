627568457dd8a7b5db62a95b818a6242685187d3
radioGiorgio <florent.delgrange@gmail.com>
2016-03-27 22:47:47 +0200

    Cooperative IA improved

--- src/Games/Nim/Players/CooperativeAI.java
+++ src/Games/Nim/Players/CooperativeAI.java
@@ -3,7 +3,6 @@ package Games.Nim.Players;
 import Core.NameAvatar;
 import Games.Nim.Moves.Move;
 import Games.Nim.Moves.MoveToken;
-import Games.Nim.Player;
 
 import java.util.List;
 
@@ -14,7 +13,7 @@ public class CooperativeAI extends RandomAI{
 
     /**
      * Creates a cooperative AI.
-     * You must respect the order : This AI has to play just before a WinnerAI
+     * You must respect the order : This AI must play just before a WinnerAI
      * to allow it to win.
      *
      * @param avatar The name of the AI.
@@ -29,10 +28,10 @@ public class CooperativeAI extends RandomAI{
         int position = getBoard().getTokenPosition().getI();
         if (position % (maxLeap + 1) == 0 || (position % (maxLeap + 1) == position))
             return super.pickMove();
-        else if (((position + 1) % (maxLeap + 1)) == 0)
-            return new MoveToken((position%(maxLeap+1))-1);
+        else if (((position - 1) % (maxLeap + 1)) == 0)
+            return new MoveToken(2);
         else
-            return new MoveToken((position%(maxLeap+1))+1);
+            return new MoveToken(1);
     }
 
     @Override
